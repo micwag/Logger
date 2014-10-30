e5b9f63c55db903bfa4643ab0c821ce7f37f53f6
Michael Wagner <michael-wagner@micwag.de>
2014-10-30 19:12:04 +0100

    Initial commit

---
+++ de/micwag/LogLevel.php
@@ -0,0 +1,11 @@
+<?php
+namespace de\micwag;
+
+abstract class LogLevel
+{
+	const INFO = 1;
+	const NOTICE = 2;
+	const WARNING = 3;
+	const ERROR = 4;
+	const FATAL_ERROR = 5;
+} 
---
+++ de/micwag/Logger.php
@@ -0,0 +1,116 @@
+<?php
+namespace de\micwag;
+
+/**
+ * Logging class
+ * Uses singleton pattern
+ *
+ * @package de\rrby1
+ */
+class Logger
+{
+	/**
+	 * Logger instance
+	 * @var Logger
+	 */
+	protected static $instance;
+	protected static $isInitialized = false;
+
+	protected $logFilePath;
+	protected $logLevel;
+
+	/**
+	 * @param string $logFilePath
+	 * @param int    $logLevel
+	 *
+	 * @throws \NotFoundException
+	 */
+	protected function __construct($logFilePath, $logLevel)
+	{
+		$this->logFilePath = $logFilePath;
+		$this->logLevel    = $logLevel;
+
+		Logger::$isInitialized = true;
+	}
+
+	/**
+	 * @param int    $logLevel
+	 * @param string $message
+	 */
+	protected function log($logLevel, $message)
+	{
+		if ($logLevel >= $this->logLevel) {
+			$date      = date('Y-m-d H:i:s');
+			$logString = "$date;$logLevel;$message\n";
+
+			try {
+				$file = fopen($this->logFilePath, 'a');
+				fwrite($file, $logString);
+				fclose($file);
+			} catch (\Exception $e) {
+				echo "Cannot write into log file.";
+			}
+		}
+	}
+
+	/**
+	 * Initializes the Logger class
+	 *
+	 * @param string $logFilePath
+	 * @param int    $logLevel
+	 */
+	public static function initialize($logFilePath, $logLevel)
+	{
+		Logger::$instance = new Logger($logFilePath, $logLevel);
+	}
+
+	/**
+	 * Logs the given message with INFO level
+	 *
+	 * @param string $message
+	 */
+	public static function info($message)
+	{
+		Logger::$instance->log(LogLevel::INFO, $message);
+	}
+
+	/**
+	 * Logs the given message with NOTICE level
+	 *
+	 * @param string $message
+	 */
+	public static function notice($message)
+	{
+		Logger::$instance->log(LogLevel::NOTICE, $message);
+	}
+
+	/**
+	 * Logs the given message with WARNING level
+	 *
+	 * @param string $message
+	 */
+	public static function warning($message)
+	{
+		Logger::$instance->log(LogLevel::WARNING, $message);
+	}
+
+	/**
+	 * Logs the given message with ERROR level
+	 *
+	 * @param string $message
+	 */
+	public static function error($message)
+	{
+		Logger::$instance->log(LogLevel::ERROR, $message);
+	}
+
+	/**
+	 * Logs the given message with FATAL_ERROR level
+	 *
+	 * @param string $message
+	 */
+	public static function fatalError($message)
+	{
+		Logger::$instance->log(LogLevel::FATAL_ERROR, $message);
+	}
+} 
